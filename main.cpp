971677aba94c9933d3324c0ba64009c40bd129c3
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2022-11-29 09:45:04 +0100

    creata struttura del loop nel main

--- main.cpp
+++ main.cpp
@@ -27,18 +27,32 @@ int main() {
 
         switch (selectedAction) {
             case 1:
+                std::cout << "1";
+                break;
 
             case 2:
+                std::cout << "2";
+                break;
 
             case 3:
+                std::cout << "3";
+                break;
 
             case 4:
+                std::cout << "4";
+                break;
 
             case 5:
+                std::cout << "5";
+                break;
 
             case 6:
+                std::cout << "6";
+                break;
 
             case 7:
+                std::cout << "7";
+                break;
 
             case 8:
                 exit(0);
